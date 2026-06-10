645b75ad0378d32ef7cb417b9f1e73d04850295c
Nwokedi Chigozirim <nwokedichigozirim747@gmail.com>
2026-06-10 01:20:09 +0100

    small adjustments

--- auth.js
+++ auth.js
@@ -18,6 +18,7 @@ const getFirebasePrivateKey = () => {
 };
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  trustHost: true,
   providers: [
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
